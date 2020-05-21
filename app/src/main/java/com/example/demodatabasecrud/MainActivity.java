f965ddacefd25c7fcd98de6c34dae0837416b866
18016375 <18016375@myrp.edu.sg>
2020-05-21 10:38:50 +0800

    activities 1-10000000000

--- app/src/main/java/com/example/demodatabasecrud/MainActivity.java
+++ app/src/main/java/com/example/demodatabasecrud/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.demodatabasecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
