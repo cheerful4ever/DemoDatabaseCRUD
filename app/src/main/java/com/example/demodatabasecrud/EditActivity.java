44cb708a4003ee7d1928979c2cf3f5657f409bab
18016375 <18016375@myrp.edu.sg>
2020-05-21 11:01:30 +0800

    activities 1-10000000000

--- app/src/main/java/com/example/demodatabasecrud/EditActivity.java
+++ app/src/main/java/com/example/demodatabasecrud/EditActivity.java
@@ -2,13 +2,36 @@ package com.example.demodatabasecrud;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditActivity extends AppCompatActivity {
+
+    TextView tvID;
+    EditText etContent;
+    Button btnUpdate, btnDelete;
+    Note data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        //initialize the variables with UI here
+        btnUpdate = findViewById(R.id.btnUpdate);
+        btnDelete = findViewById(R.id.btnDelete);
+        etContent = findViewById(R.id.etContent);
+        tvID = findViewById(R.id.tvID);
+
+        Intent i = getIntent();
+        data = (Note) i.getSerializableExtra("data");
+
+        tvID.setText("ID: " + data.getId());
+        etContent.setText(data.getNoteContent());
+
     }
 }
+
